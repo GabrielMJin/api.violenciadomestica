99da53448d042b6dcc37d1c9f0ca56de9d83ad0f
Gabriel Moura <gabrielmoralgs@gmail.com>
2021-10-28 09:47:54 -0300

    retirada de seeder

--- src/dados/dados.seeder.ts
+++ src/dados/dados.seeder.ts
@@ -1,19 +1,19 @@
 /* eslint-disable prettier/prettier */
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-import { DataFactory, Seeder } from 'nestjs-seeder';
-import { Dado, DadoDocument } from './schemas/dado.schema';
+//import { InjectModel } from '@nestjs/mongoose';
+//import { Model } from 'mongoose';
+//import { DataFactory, Seeder } from 'nestjs-seeder';
+//import { Dado, DadoDocument } from './schemas/dado.schema';
 
-export class DadosSeeder implements Seeder {
-    constructor(@InjectModel(Dado.name) private dadoModel: Model<DadoDocument>) {}
+//export class DadosSeeder implements Seeder {
+//    constructor(@InjectModel(Dado.name) private dadoModel: Model<DadoDocument>) {}
 
-    drop(): Promise<any> {
-        return this.dadoModel.deleteMany({}) as any;
-    }
+//    drop(): Promise<any> {
+//        return this.dadoModel.deleteMany({}) as any;
+//    }
 
-    seed(): Promise<any> {
-        const dados: any = DataFactory.createForClass(Dado).generate(50);
+    //seed(): Promise<any> {
+        //const dados: any = DataFactory.createForClass(Dado).generate(50);
 
-        return this.dadoModel.insertMany(dados);
-    }
-}
+        //return this.dadoModel.insertMany(dados);
+    //}
+//}
--- src/dados/seeder.ts
+++ src/dados/seeder.ts
@@ -1,12 +1,12 @@
 /* eslint-disable prettier/prettier */
-import { MongooseModule } from '@nestjs/mongoose';
-import { DadosSeeder } from './dados.seeder';
-import { seeder } from 'nestjs-seeder';
-import { Dado, DadoSchema } from './schemas/dado.schema';
+//import { MongooseModule } from '@nestjs/mongoose';
+//import { DadosSeeder } from './dados.seeder';
+//import { seeder } from 'nestjs-seeder';
+//import { Dado, DadoSchema } from './schemas/dado.schema';
 
-seeder({
-  imports: [
-    MongooseModule.forRoot('mongodb+srv://sirjinluffy:A9d8z1fTEidZkrYs@ativ2.pjk5x.mongodb.net/test'),
-    MongooseModule.forFeature([{ name: Dado.name, schema: DadoSchema }]),
-  ],
-}).run([DadosSeeder]);
+//seeder({
+//  imports: [
+//    MongooseModule.forRoot('mongodb+srv://sirjinluffy:A9d8z1fTEidZkrYs@ativ2.pjk5x.mongodb.net/test'),
+//    MongooseModule.forFeature([{ name: Dado.name, schema: DadoSchema }]),
+//  ],
+//}).run([DadosSeeder]);
